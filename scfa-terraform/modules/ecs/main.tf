cb6002e5061391b57d5e31a62cbc2221061aa828
srishti-ranjan <srishtiranjan2910@gmail.com>
2026-05-25 18:18:27 +0000

    Add ECS autoscaling

--- scfa-terraform/modules/ecs/main.tf
+++ scfa-terraform/modules/ecs/main.tf
@@ -60,3 +60,31 @@ resource "aws_ecs_service" "this" {
 
   depends_on = [aws_cloudwatch_log_group.this]
 }
+
+
+resource "aws_appautoscaling_target" "ecs" {
+  max_capacity       = var.max_capacity
+  min_capacity       = var.min_capacity
+  resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.this.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "cpu" {
+  name               = "cpu-scaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value = 50.0
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 60
+  }
+}
